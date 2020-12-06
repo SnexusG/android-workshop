0bd99aa6f562e41075e62dad40a3d0b60cb08b2e
SnexusG <swapnilgore029@gmail.com>
2020-12-06 09:44:23 +0530

    removed imports

--- Notes_onlyBackend/app/src/main/java/com/example/notes/MainActivity.java
+++ Notes_onlyBackend/app/src/main/java/com/example/notes/MainActivity.java
@@ -1,21 +1,8 @@
 package com.example.notes;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import com.example.notes.model.Note;
-import com.example.notes.viewmodel.NoteViewModel;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
