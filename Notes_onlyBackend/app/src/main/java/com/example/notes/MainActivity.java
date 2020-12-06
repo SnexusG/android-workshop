4687df78f76c851c0c48780214765f5ae5264c6d
SnexusG <swapnilgore029@gmail.com>
2020-12-06 09:42:54 +0530

    removed add asset

--- Notes_onlyBackend/app/src/main/java/com/example/notes/MainActivity.java
+++ Notes_onlyBackend/app/src/main/java/com/example/notes/MainActivity.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
